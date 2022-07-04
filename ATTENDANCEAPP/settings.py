9fd257881f2b2a029e41d77930ff9512e64ee850
abdulazeez-sodiq <adetunji639@gmail.com>
2022-07-04 13:01:57 +0100

    nc

--- ATTENDANCEAPP/settings.py
+++ ATTENDANCEAPP/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-3*eawz=nb8dgo5#con87!lm8$ca*ixm+tjiku$*fqpjwss&9)d
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1','attendanceterm.herokuapp.com']
+ALLOWED_HOSTS = []
 
 
 # Application definition
