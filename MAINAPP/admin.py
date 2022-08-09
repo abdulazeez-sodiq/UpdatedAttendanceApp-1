a4349977365d84bbef117618fd9fc6f7748725a5
abdulazeez-sodiq <adetunji639@gmail.com>
2022-08-09 15:00:05 +0100

    initial commit

--- MAINAPP/admin.py
+++ MAINAPP/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import FIRM, Staff
+
+admin.site.register(FIRM)
+
+admin.site.register(Staff)
--- MAINAPP/models.py
+++ MAINAPP/models.py
@@ -1,2 +1,22 @@
+from unicodedata import name
 from django.db import models
 # Create your models here.
+
+
+class FIRM(models.Model):
+    Name = models.CharField(max_length=200, null=True)
+    Email = models.CharField(max_length=200, null=True)
+    HowYouKnowUs = models.CharField(max_length=200, null=True, default='')
+    ConnectionStatus = models.CharField(max_length=200, null=True, default='')
+    Agree = models.CharField(max_length=200, null=True, default='')
+
+    def __str__(self):
+        return self.Name
+
+class Staff(models.Model):
+    Name = models.CharField(max_length=200, null=True)
+    ID_Number = models.PositiveIntegerField(null=False)
+    user = models.ForeignKey(FIRM, null=True, on_delete= models.SET_NULL)
+
+    def __str__(self):
+        return self.Name
